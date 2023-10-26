ca296d99542741ee88e9ac54bfd505433fdd8a3c
babluMazumder <bablupub@gmail.com>
2023-10-26 22:55:45 +0600

    update lang

---
+++ app/View/Composers/LangComposer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Composers;
+
+use App\Models\Post;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\Session;
+use App\Repositories\Language\LanguageInterface;
+
+class LangComposer
+{
+    protected $repoLang;
+
+    public function __construct(LanguageInterface $repoLang)
+    {
+
+        $this->repoLang     = $repoLang;
+    }
+
+    /**
+     * @param View $view
+     */
+    public function compose(View $view)
+    {
+
+        $view->with('languages', $this->repoLang->activelang());
+    }
+}
